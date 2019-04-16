89d00883463bc7b7c6d6bff3e9528afc3c587416
RudsonA <rudson.augustin@yahoo.com>
2019-04-16 17:30:00 -0400

    easy math

---
+++ py/ReturnNegative.py
@@ -0,0 +1,15 @@
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+# The number can be negative already, in which case no change is required.
+
+# Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+def make_negative( number ):
+    if number == 0:
+    	return 0
+    if number < 0:
+    	return number
+
+    number = number - (number * 2)
+
+    return number
